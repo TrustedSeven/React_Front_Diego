f5851bbebe09ae24c054c9e87d4b8164e4c32d37
TrustedSeven <castaneda.chistian0215@gmail.com>
2023-07-20 16:14:52 -0500

    sidebar cookie left

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,23 +1,18 @@
 import React from "react";
 
 import Header from "../layout/Header";
+import Content from "../layout/Content";
 
 export default function Home() {
   return (
     <div className="flex flex-row p-5 min-h-screen">
       <div className="bg-[#FF0000] ">aaaaaaaa00000000000000000</div>
-      <div>
+      <div className="grow p-5">
         <div>
           <Header />
         </div>
         <div>
-          <div>Hello</div>
-          <div className="flex flex-row">
-            <div>Empresa</div>
-            <div>Personal</div>
-            <div>Vehiculos</div>
-            <div>Maquinaria</div>
-          </div>
+          <Content />
         </div>
       </div>
     </div>
